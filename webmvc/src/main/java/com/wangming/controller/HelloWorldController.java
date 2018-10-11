6c8958dab75fdac912bc8902f72cc30aa8e8787b
ASYCN\Ming.Wang <1779016007@qq.com>
2018-10-11 14:13:24 +0800

    对对对

---
+++ webmvc/src/main/java/com/wangming/controller/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.wangming.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * HelloWorld {@link Controller}
+ *
+ * @author 小马哥
+ * @since 2018/5/20
+ */
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/index")
+    public String index() {
+//        model.addAttribute("acceptLanguage",acceptLanguage);
+//        model.addAttribute("jsessionId",jsessionId);
+//        model.addAttribute("message","Hello,World");
+        return "index";
+    }
+
+}
